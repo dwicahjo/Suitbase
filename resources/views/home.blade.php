85eb97b3762babfd50d155a50b42c7554b0b6349
Reza Briyan Pratama <RezaBriyanP@Rezas-MacBook-Air.local>
2016-05-19 00:12:47 +0700

    deleted duplicates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,13 @@
 @extends('layoutTemplate')
 @section('content')
-
-<div id="page-wrapper">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="welcome">WELCOME TO SUITBASE, </h1>
-            <h1><p class="name"> {{ Auth::user()->name }} ! </p></h1>
-        </div>
-        <!-- /.col-lg-12 -->
-    </div>
- 
-    <!-- /#page-wrapper -->
-    @endsection
+	<div id="page-wrapper">
+	    <div class="row">
+	        <div class="col-lg-12">
+	            <h1 class="welcome">WELCOME TO SUITBASE, </h1>
+	            <h1><p class="name"> {{ Auth::user()->name }} ! </p></h1>
+	        </div>
+	        <!-- /.col-lg-12 -->
+	    </div>
+	</div>
+	 <!-- /#page-wrapper -->
+@endsection
